7d8159d515f707f1d64bfd6da5f59a4571a88ea1
hskim <hskim@ypt.kr>
2024-10-22 14:55:22 +0900

    Modified

--- cont_temp/src/App.tsx
+++ cont_temp/src/App.tsx
@@ -1,16 +1,40 @@
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "./store/store";
 import { increment, decrement } from "./store/exampleSlice";
+import { motion } from "framer-motion";
 
 function App() {
   const count = useSelector((state: RootState) => state.example.value);
   const dispatch = useDispatch();
 
   return (
-    <div className="App">
-      <h1>Counter : {count}</h1>
-      <button onClick={() => dispatch(increment())}>+</button>
-      <button onClick={() => dispatch(decrement())}>-</button>
+    <div className="w-full h-screen flex items-center justify-center space-x-4">
+      <motion.p
+        initial={{ scale: 0.8, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{ duration: 0.3 }}
+        className="text-2xl font-bold"
+      >
+        Counter: {count}
+      </motion.p>
+
+      <motion.button
+        className="bg-blue-500 text-white py-2 px-4 rounded-lg shadow-md hover:bg-blue-600"
+        onClick={() => dispatch(increment())}
+        whileTap={{ scale: 0.9 }}
+        whileHover={{ scale: 1.1 }}
+      >
+        +
+      </motion.button>
+
+      <motion.button
+        className="bg-red-500 text-white py-2 px-4 rounded-lg shadow-md hover:bg-red-600"
+        onClick={() => dispatch(decrement())}
+        whileTap={{ scale: 0.9 }}
+        whileHover={{ scale: 1.1 }}
+      >
+        -
+      </motion.button>
     </div>
   );
 }
--- cont_temp/src/main.tsx
+++ cont_temp/src/main.tsx
@@ -2,13 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "@radix-ui/themes/styles.css";
 import { Provider } from "react-redux";
 import store from "./store/store.ts";
+import { Theme } from "@radix-ui/themes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Theme>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Theme>
   </StrictMode>
 );
